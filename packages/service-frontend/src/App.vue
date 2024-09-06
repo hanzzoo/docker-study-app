26ac87be64dae7c9d50a8cc75b886b91edc5b786
hanzzoo <92495841+hanzzoo@users.noreply.github.com>
2024-09-06 18:03:53 +0900

    feat(service-backend): fetch /sample api

--- packages/service-frontend/src/App.vue
+++ packages/service-frontend/src/App.vue
@@ -1,14 +1,28 @@
 <script setup>
+  import axios from 'axios'
+  import { onMounted, ref } from 'vue';
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/sample')
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const message = ref('')
+
+  onMounted(async() => {
+    const result = await fetchData()
+    message.value = result.message
+  })
 </script>
 
 <template>
-  <header>
-    sample app
-  </header>
   <main>
     <div class="wrapper">
-      hello world
+      {{  message }}
     </div>
   </main>
 </template>
